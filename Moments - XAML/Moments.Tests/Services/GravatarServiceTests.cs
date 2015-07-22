6c1a47d172a5fc5763b845d766d2e0cec3cb6882
Tyler Vanderhoef <tyler.j.vanderhoef@gmail.com>
2015-07-21 23:12:37 -0700

    Init include of project

---
+++ Moments - XAML/Moments.Tests/Services/GravatarServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+using Moments;
+using FluentAssertions;
+
+namespace Moments.Tests
+{
+	public class GravatarServiceTests
+	{
+		[TestFixture]
+		public class CalculateUrl
+		{
+			[Test]
+			public void ShouldCalculateCorrectly()
+			{
+				var url = GravatarService.CalculateUrl ("test@test.com");
+				url.Should ().Be ("http://www.gravatar.com/avatar.php?gravatar_id=b642b4217b34b1e8d3bd915fc65c4452");
+			}
+		}
+	}
+}
+

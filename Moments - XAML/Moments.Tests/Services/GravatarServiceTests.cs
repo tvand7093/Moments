1e9d57aa4736b9a9151758fa0d7dcb2b0ca13911
Tyler Vanderhoef <tyler.j.vanderhoef@gmail.com>
2015-07-22 00:57:21 -0700

    Test bad commit.

--- Moments - XAML/Moments.Tests/Services/GravatarServiceTests.cs
+++ Moments - XAML/Moments.Tests/Services/GravatarServiceTests.cs
@@ -14,7 +14,7 @@ namespace Moments.Tests
 			public void ShouldCalculateCorrectly()
 			{
 				var url = GravatarService.CalculateUrl ("test@test.com");
-				url.Should ().Be ("http://www.gravatar.com/avatar.php?gravatar_id=b642b4217b34b1e8d3bd915fc65c4452");
+				url.Should ().Be ("https://www.gravatar.com/avatar.php?gravatar_id=b642b4217b34b1e8d3bd915fc65c4452");
 			}
 		}
 	}

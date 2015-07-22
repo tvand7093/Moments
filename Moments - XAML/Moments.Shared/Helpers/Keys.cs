165cec6db1c6aa99dcd30881e7579fd4d5ca5cc8
Pierce Boggan <pierce@pierceboggan.com>
2015-07-21 23:16:48 -0500

    Styles Configured

---
+++ Moments - XAML/Moments.Shared/Helpers/Keys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Moments
+{
+	// TODO: Insert relevant keys from Azure and Xamarin Insights
+	public class Keys
+	{
+		public static readonly string ApplicationURL = "https://xamarinhqmoments.azure-mobile.net/";
+		public static readonly string ApplicationMobileService = "xamarinhqmoments.azure-mobile.net";
+		public static readonly string ApplicationKey = "YVRhiwqFaGvwbypUumatYrgtaPFDAh73";
+		public static readonly string ContainerURL = "https://xamarinhqmoments.blob.core.windows.net/momentsapp";
+		public static readonly string ContainerName = "momentsapp";
+		public static readonly string InsightsKey = "9a0fa9121ed4d395107267c5af631a37fe2b97f3";
+	}
+}
---
+++ Moments - XAML/Moments.Shared/Views/WelcomePage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Moments
+{
+	public partial class WelcomePage : ContentPage
+	{
+		public WelcomePage ()
+		{
+			InitializeComponent ();
+
+			SetupUserInterface ();
+			SetupEventHandlers ();
+		}
+
+		private void SetupUserInterface ()
+		{
+			NavigationPage.SetHasNavigationBar (this, false);
+		}
+
+		private void SetupEventHandlers ()
+		{
+
+		}
+	}
+}
+

8ddbf33fbc69a6f62917f063c873b16b12145ade
Tyler Vanderhoef <tyler.j.vanderhoef@gmail.com>
2015-07-22 18:19:02 -0700

    Pre merge

---
+++ Moments - XAML/Moments.Tests/ViewModels/SignUpViewModelTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+using Moq;
+
+namespace Moments.Tests.ViewModels
+{
+	[TestFixture]
+	public class SignUpViewModelTests
+	{
+		[Test]
+		public SignUpViewModelTests ()
+		{
+		}
+	}
+}
+
